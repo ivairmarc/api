f5c97aace346ebc25544c6242383cea9029c4a36
ivairmarc <ivairwork@outlook.com>
2024-07-09 16:01:57 -0400

    FastApi

---
+++ scripts.sh
@@ -0,0 +1,24 @@
+# arquivos na raiz
+touch setup.py
+touch {settings,.secrets}.toml
+touch {requirement,MANIFEST}.in
+touch Dockerfile.dev docker-compose.yaml
+
+# Imagem do banco de dados
+
+mkdir postgres
+touch postgres/{Dockerfile,create-databases.sh}
+
+# Aplicacao
+
+mkdir -p pamps/{models,routes}
+touch pamps/default.toml
+touch pamps/{__init__,cli,app,auth,db,security,config}.py
+touch pamps/models/{__init__,post,user}.py
+touch pamps/routes/{__init__,auth,post,user}.py
+
+# Testes
+
+touch test.sh
+mkdir tests
+touch tests/{__init__,conftest,test_api}.py
